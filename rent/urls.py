106d03d42dbb3953f67f9123c3e8d4ced9d70a5f
Vilen0 <v.avtandilyan@mail.ru>
2025-05-19 10:43:26 +0300

    Прокат автомобилей

--- rent/urls.py
+++ rent/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('about/', views.about, name='about'),
+    path('profile/', views.profile, name='profile'),
 ]
